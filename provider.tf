1e4d4e9d3127305fe4c2c8a589e30b0c73837d62
amarsudha9 <65554758+amarsudha9@users.noreply.github.com>
2025-01-31 14:21:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -15,5 +15,10 @@ terraform {
     storage_account_name = "devopsbackend"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "backenddevops"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
+    use_oidc             = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
+    client_id            =  ${{secrets.AZURE_CLIENT_ID}} # Can also be set via `ARM_CLIENT_ID` environment variable.
+    subscription_id      =   ${{}secrets.AZURE_SUBSCRIPTION_ID}# Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
+    tenant_id            = ${{secrets.AZURE_TENANT_ID}}  # Can also be set via `ARM_TENANT_ID` environment variable.  
+
+}
 }
