dde4947c777bfbdec1161b903129e57c2f82c2c6
amarsudha9 <65554758+amarsudha9@users.noreply.github.com>
2025-01-31 14:15:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,11 +9,11 @@ terraform {
   }
 }
 
-#     terraform {
-       backend "azurerm" {
-#        resource_group_name = "Terraform-RG"
-         storage_account_name = "devopsbackend"
-         container_name = "backenddevops"
-         key = ${{secrets.BACKENDDEVOPS}}
-       }
-     }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "Terraform-RG"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "devopsbackend"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "backenddevops"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
